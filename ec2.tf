d7cf40c267910fe261b6223107603982112e0ef6
vamsi-1670 <62005464+vamsi-1670@users.noreply.github.com>
2020-08-02 23:14:26 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -57,7 +57,6 @@ resource "aws_instance" "web" {
   key_name = "aws_key_pair.deployer-key.key_name"
   security_groups = ["aws_security_group.main.id"]
   subnet_id = "aws_subnet.main.id"
-   tags {
-      Name = "Test-AMI"
-       }
+  Name = "Test-AMI"
+ 
 }
