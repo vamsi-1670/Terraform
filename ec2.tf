33b9b118a3862d5917d9040d0ca18080a3b200ea
vamsi-1670 <62005464+vamsi-1670@users.noreply.github.com>
2020-08-02 23:44:19 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,13 +1,13 @@
 
 provider "aws" {
-  access_key = "AKIA35K4OV7XD2HWZUTO"
-  secret_key = "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"
+  access_key = "<access_key_here>"
+  secret_key = "<secret_key_here>"
   region = "us-west-2"
 }
 ## Resource to provision public key
 resource "aws_key_pair" "deployer-key" {
   key_name   = "deployer-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDhSRwfUqIoUqp6F6L8uRSws1cqU1Vpe2L0qZ7IXuGDSFeXiNstL+3ovP3QvDPVMMuEcXu+TJcGebKx6grtabKA93sdiYmQKD8iBZ5Q+5m0IaR45SSoa6jgq09Tust4uZIl5Blx6yQWg8dLSGxjAVqetTN1k2ksG+WJqb0QpqBb4SnQgrgSCEOS0BqNnCnEySawjYrbn4A/7WKJMWRwOUiM3Mmf84mIt85cGR3qdqHT8jGqZRdpjLfCu1qxQmun7yCvIUZZqiajaLxyVHnhtS72xBNZpp7ArFnIn3RdGEWBC1HlMg0P8VBCa7d47RTHJMqkVFqA1EHpDrmlLpljhyUT root@vagrant"
+  public_key = "<ssh-key>"
   }
 ## Resource to provision VPC 
 resource "aws_vpc" "main" {
