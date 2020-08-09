60dfc53580dfa5618a15baea52539e2b7b8ce378
vamsi-1670 <62005464+vamsi-1670@users.noreply.github.com>
2020-08-09 20:32:55 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -13,17 +13,12 @@ resource "aws_key_pair" "deployer-key" {
 resource "aws_vpc" "main" {
   cidr_block       = "192.168.0.0/16"
   instance_tenancy = "default"
-  tags = {
-    Name = "test-vpc"
-  }
 }
 ## Resource to provision Subnet
 resource "aws_subnet" "main" {
   vpc_id     = "aws_vpc.main.id"
   cidr_block = "192.168.0.0/24"
-  tags = {
-    Name = "test-subnet"
-  }
+  
 }
 ## Resource to create Security group with inbound traffic ports 22 & 80.
 resource "aws_security_group" "main" {
@@ -45,17 +40,13 @@ resource "aws_security_group" "main" {
     cidr_blocks = ["192.168.0.0/24"]
 }
 
-  tags = {
-    Name = "allow_tls"
-  }
-
 }
 ## Resource to provision ec2 instance
 resource "aws_instance" "main" {
   ami = "ami-0d1cd67c26f5fca19"
   instance_type = "t2.micro"
   key_name = "aws_key_pair.deployer-key.key_name"
-  security_groups = ["aws_security_group.main.id"]
+  security_groups = ["aws_security_group.main.id"
   subnet_id = "aws_subnet.main.id"
   
 }
