659b01c823df797cc5602ccd2814ec955a07106b
vamsi-1670 <62005464+vamsi-1670@users.noreply.github.com>
2020-08-09 19:50:26 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "AWS_ACCESS_KEY" {
- description = "The AWS access key."
- default     = "AKIA35K4OV7XFP24ZRXQ"
-}
-
-variable "AWS_SECRET_KEY" {
- description = "The AWS secret key."
- default     = "P3vBmUuheXltJu+c0g5jfSc9lEvz5HjDyfRfAREF"
-}
