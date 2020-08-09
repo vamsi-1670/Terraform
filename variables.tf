64e2ddf444256d3ca83148a0c570fa49a164ba22
vamsi-1670 <62005464+vamsi-1670@users.noreply.github.com>
2020-08-09 19:48:39 +0530

    Update and rename vars.tf to variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "AWS_ACCESS_KEY" {
+ description = "The AWS access key."
+ default     = "AKIA35K4OV7XFP24ZRXQ"
+}
+
+variable "AWS_SECRET_KEY" {
+ description = "The AWS secret key."
+ default     = "P3vBmUuheXltJu+c0g5jfSc9lEvz5HjDyfRfAREF"
+}
